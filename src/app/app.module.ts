94484ae63591c391241c98e341cd66d6c0e1e214
Thiago <ppcp.jairmoveis@gmail.com>
2025-02-11 09:39:41 -0300

    Atualizando as dependências do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';  // Certifique-se de que o HttpClientModule está sendo importado
-import { ReactiveFormsModule } from '@angular/forms';  // Certifique-se de que o ReactiveFormsModule está sendo importado
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
@@ -20,7 +19,9 @@ import { FooterComponent } from './components/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
